3c16fed5b4533efbbda857aa05de6ed0eb9ee9e8
0x1f595 <0x1f595@users.noreply.github.com>
2018-06-17 09:44:13 -0600

    Overhauling code and adding new site

--- README.md
+++ README.md
@@ -2,19 +2,28 @@ dandl
 =====
 *A simple Danbooru/Gelbooru/Shimmie scraper tool*
 
-Install Python 3, the dependencies, and run the script~!
+# Setup
+
+Install Python 3 with pip, then install the dependencies:
 
 ```bash
-pip install -r requirements.txt
-python dandl.py <provider> <search>
+pip install -r requirements.txt --target lib
 ```
 
 If your default system Python is version 2, try `python3` and `pip3` instead.
 
-If the `install -r` doesn't work, this might work on Ubuntu:
+If the `pip install ...` doesn't work, this might work on Ubuntu:
 
 ```bash
-sudo -H python3 -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt --target lib
+```
+
+# Usage
+
+Just run the script~!
+
+```bash
+./dandl.py <provider> <search-tag> [search-tag ...]
 ```
 
 Provider can be a hostname or short code. A list of all supported providers is in the help menu, accessible via the `-h` flag.
--- dandl.py
+++ dandl.py
@@ -1,56 +1,216 @@
 #!/usr/bin/env python
+"""
+dandl - Danbooro/Gelbooru/Shimmie downloader
+"""
+
+# Allow local package installs
+import sys
 import os
-from appdirs import user_config_dir
+sys.path.append(os.getcwd() + os.sep + 'lib')
+
 import configparser
 import argparse
-from urllib import parse, request
 import xml.etree.ElementTree as ET
 import json
 import time
-from sys import stderr
 import textwrap
+from urllib import parse, request
+from sys import stderr
+from appdirs import user_config_dir
 
-"""
-Print a string to stderr
-"""
-def print_err(*args, **kwargs):
-    print(*args, file=stderr, **kwargs)
+images = []
 
-# Read configuration
-config_file = user_config_dir() + os.sep + 'dandl.conf'
-config = configparser.ConfigParser()
-if os.path.isfile(config_file):
-    config.read(config_file)
+def main():
+    """
+    Run the scraper
+    """
 
-supported_sites = ['danbooru.donmai.us', 'safebooru.org', 'rule34.paheal.net',
-                   'shimmie.katawa-shoujo.com']
-max_filename_len = 240
+    # Read configuration
+    config_file = user_config_dir() + os.sep + 'dandl.conf'
+    config = configparser.ConfigParser()
+    if os.path.isfile(config_file):
+        config.read(config_file)
 
-# Read arguments
-parser = argparse.ArgumentParser(
-    description='Bulk download images from a Danbooru/Gelbooru/Shimmie site',
-    epilog='supported providers: ' + ' '.join(supported_sites))
-parser.add_argument('--dir', help='Directory to save images to')
-parser.add_argument('--limit', type=int, help='Max number of images to download')
-parser.add_argument('--nd', action='store_true',
-    help='Skip downloading images, only displaying URLs')
-parser.add_argument('provider', help='Provider to download from')
-parser.add_argument('tag', nargs='+', help='Tags to search for')
-args = parser.parse_args()
+    supported_sites = ['danbooru.donmai.us', 'safebooru.org', 'rule34.paheal.net',
+                       'rule34.xxx', 'shimmie.katawa-shoujo.com']
+    max_filename_len = 240
 
-# Prepare output directory
-dir = config['DEFAULT'].get('savedir')
-if dir:
-    dir = os.path.expanduser(dir)
-    if args.nd == False:
-        os.makedirs(dir, exist_ok=True)
-else:
-    dir = os.curdir
+    # Read arguments
+    parser = argparse.ArgumentParser(
+        description='Bulk download images from a Danbooru/Gelbooru/Shimmie site',
+        epilog='supported providers: ' + ' '.join(supported_sites))
+    parser.add_argument('--dir', help='Directory to save images to')
+    parser.add_argument('--limit', type=int, help='Max number of images to download')
+    parser.add_argument('--nd', action='store_true',
+                        help='Skip downloading images, only displaying URLs')
+    parser.add_argument('provider', help='Provider to download from')
+    parser.add_argument('tag', nargs='+', help='Tags to search for')
+    args = parser.parse_args()
 
-"""
-Add images from a Danbooru JSON response
-"""
-def addDanbooruPosts(response, url):
+    # Prepare output directory
+    imgdir = config['DEFAULT'].get('savedir')
+    if imgdir:
+        imgdir = os.path.expanduser(imgdir)
+        if not args.nd:
+            os.makedirs(imgdir, exist_ok=True)
+    else:
+        imgdir = os.curdir
+
+    # Find images by provider
+    if args.provider == 'danbooru.donmai.us' or args.provider == 'danbooru':
+        print_err('Searching for images...')
+        url = 'https://danbooru.donmai.us/post/index.json?tags='
+        query = parse.quote_plus(' '.join(args.tag))
+        headers = {'User-Agent': 'dandl/1.0'} # Danbooru blocks non-UA'd reqs
+        req = request.Request(url + query, None, headers)
+        res = request.urlopen(req)
+        root = json.loads(res.read().decode("utf-8"))
+
+        limit = args.limit
+        if not limit and config['DEFAULT'].get('limit'):
+            limit = int(config['DEFAULT'].get('limit'))
+        if not limit:
+            limit = 1e7
+        page = 1
+        while len(images) < int(limit):
+            count = add_danbooru_posts(root, url)
+            if not count:
+                break
+            page += 1
+            req = request.Request(url + query + '&page=' + str(page), None, headers)
+            res = request.urlopen(req)
+            root = json.loads(res.read().decode("utf-8"))
+            if not root:
+                break
+            time.sleep(0.5)
+
+    elif args.provider == 'safebooru.org' or args.provider == 'safebooru':
+        print_err('Searching for images...')
+        url = 'https://safebooru.org/index.php?page=dapi&s=post&q=index&'
+        query = parse.quote_plus(' '.join(args.tag))
+        req = request.urlopen(url + query)
+        root = ET.fromstring(req.read())
+
+        limit = args.limit
+        if not limit and config['DEFAULT'].get('limit'):
+            limit = int(config['DEFAULT'].get('limit'))
+        if not limit:
+            limit = root.get('count')
+        while len(images) < int(limit):
+            count = add_gelbooru_posts(root, url)
+            if not count:
+                break
+            req = request.urlopen(url + query + '&offset=' + str(len(images)))
+            root = ET.fromstring(req.read())
+            time.sleep(0.5)
+
+    elif args.provider == 'rule34.paheal.net' or args.provider == 'r34':
+        print_err('Searching for images...')
+        xmlns = {'media': 'http://search.yahoo.com/mrss',
+                 'atom': 'http://www.w3.org/2005/Atom'}
+
+        url = 'https://rule34.paheal.net/rss/images/'
+        tags = parse.quote(' '.join(args.tag))
+        req = request.urlopen(url + tags + '/1')
+        root = ET.fromstring(req.read())
+
+        limit = args.limit
+        if not limit and config['DEFAULT'].get('limit'):
+            limit = int(config['DEFAULT'].get('limit'))
+        if not limit:
+            limit = 1e7
+
+        while len(images) < int(limit):
+            next_pg = None
+            channel = root.find('channel')
+            count = add_shimmie_posts(channel, url, xmlns)
+            if not count:
+                break
+            for link in channel.findall('atom:link', xmlns):
+                if link.get('rel') == 'next':
+                    next_pg = parse.urljoin(url, link.get('href'))
+                    time.sleep(0.5)
+                    req = request.urlopen(next_pg)
+                    root = ET.fromstring(req.read())
+                    break
+            if not next_pg:
+                break
+
+    elif args.provider == 'rule34.xxx' or args.provider == 'r34x':
+        print_err('Searching for images...')
+        url = 'https://rule34.xxx/index.php?page=dapi&s=post&q=index&'
+        query = parse.quote_plus(' '.join(args.tag))
+        req = request.urlopen(url + query)
+        root = ET.fromstring(req.read())
+
+        limit = args.limit
+        if not limit and config['DEFAULT'].get('limit'):
+            limit = int(config['DEFAULT'].get('limit'))
+        if not limit:
+            limit = root.get('count')
+        while len(images) < int(limit):
+            count = add_gelbooru_posts(root, url)
+            if not count:
+                break
+            req = request.urlopen(url + query + '&offset=' + str(len(images)))
+            root = ET.fromstring(req.read())
+            time.sleep(0.5)
+
+    elif args.provider == 'shimmie.katawa-shoujo.com' or args.provider == 'ks':
+        print_err('Searching for images...')
+        xmlns = {'media': 'http://search.yahoo.com/mrss/',
+                 'atom': 'http://www.w3.org/2005/Atom'}
+
+        url = 'https://shimmie.katawa-shoujo.com/rss/images/'
+        tags = parse.quote(' '.join(args.tag))
+        req = request.urlopen(url + tags + '/1')
+        root = ET.fromstring(req.read())
+
+        limit = args.limit
+        if not limit and config['DEFAULT'].get('limit'):
+            limit = int(config['DEFAULT'].get('limit'))
+        if not limit:
+            limit = 1e7
+
+        while len(images) < int(limit):
+            next_pg = None
+            channel = root.find('channel')
+            count = add_shimmie_posts(channel, url, xmlns)
+            if not count:
+                break
+            for link in channel.findall('atom:link', xmlns):
+                if link.get('rel') == 'next':
+                    next_pg = parse.urljoin(url, link.get('href'))
+                    time.sleep(0.5)
+                    req = request.urlopen(next_pg)
+                    root = ET.fromstring(req.read())
+                    break
+            if not next_pg:
+                break
+
+    else:
+        print_err('The specified provider is not supported.')
+
+    # Download images
+    print_err('%u images found' % len(images))
+    for image in images:
+        print(image['url'])
+        if not args.nd:
+            ireq = request.urlopen(image['url'])
+            basename = image['name']
+            if len(basename) > max_filename_len:
+                _, ext = os.path.splitext(basename)
+                basename = textwrap.shorten(basename, width=max_filename_len, placeholder=ext)
+            filename = imgdir.rstrip(os.sep) + os.sep + basename
+            if not os.path.isfile(filename):
+                with open(filename, 'xb') as file:
+                    file.write(ireq.read())
+            time.sleep(0.5)
+
+def add_danbooru_posts(response, url):
+    """
+    Add images from a Danbooru JSON response
+    """
     count = 0
     for entry in response:
         if not 'file_url' in entry:
@@ -65,10 +225,10 @@ def addDanbooruPosts(response, url):
         count = count + 1
     return count
 
-"""
-Add images from a Gelbooru XML feed
-"""
-def addGelbooruPosts(posts, url):
+def add_gelbooru_posts(root, url):
+    """
+    Add images from a Gelbooru XML feed
+    """
     count = 0
     for post in root.findall('post'):
         file_url = parse.urljoin(url, post.get('file_url'))
@@ -80,13 +240,13 @@ def addGelbooruPosts(posts, url):
         count = count + 1
     return count
 
-"""
-Add images from a Shimmie RSS 2.0 feed
-"""
-def addShimmiePosts(channel, url, ns):
+def add_shimmie_posts(channel, url, xmlns):
+    """
+    Add images from a Shimmie RSS 2.0 feed
+    """
     count = 0
     for image in channel.findall('item'):
-        file_url = image.find('media:content', ns).get('url')
+        file_url = image.find('media:content', xmlns).get('url')
         file_url = parse.urljoin(url, file_url)
         images.append({
             'id': image.find('guid').text.split('/')[-1],
@@ -96,135 +256,12 @@ def addShimmiePosts(channel, url, ns):
         count = count + 1
     return count
 
-# Find images by provider
-images = []
+def print_err(*args, **kwargs):
+    """
+    Print a string to stderr
+    """
+    print(*args, file=stderr, **kwargs)
 
-if args.provider == 'danbooru.donmai.us' or args.provider == 'danbooru':
-    print_err('Searching for images...')
-    url = 'https://danbooru.donmai.us/post/index.json?tags='
-    query = parse.quote_plus(' '.join(args.tag), [])
-    headers = {'User-Agent': 'dandl/1.0'} # Danbooru blocks non-UA'd reqs
-    req = request.Request(url + query,None, headers)
-    res = request.urlopen(req)
-    root = json.loads(res.read().decode("utf-8"))
-
-    limit = args.limit
-    if not(limit) and config['DEFAULT'].get('limit'):
-        limit = int(config['DEFAULT'].get('limit'))
-    if not(limit):
-        limit = 1e7
-    pg = 1
-    while len(images) < limit:
-        count = addDanbooruPosts(root, url)
-        if not(count):
-            break
-        pg += 1
-        req = request.Request(url + query + '&page=' + str(pg), None, headers)
-        res = request.urlopen(req)
-        root = json.loads(res.read().decode("utf-8"))
-        if not len(root):
-            break
-        time.sleep(0.5)
-
-elif args.provider == 'safebooru.org' or args.provider == 'safebooru':
-    print_err('Searching for images...')
-    url = 'https://safebooru.org/index.php?page=dapi&s=post&q=index&'
-    query = parse.quote_plus(' '.join(args.tag), [])
-    req = request.urlopen(url + query)
-    root = ET.fromstring(req.read())
-
-    limit = args.limit
-    if not(limit) and config['DEFAULT'].get('limit'):
-        limit = int(config['DEFAULT'].get('limit'))
-    if not(limit):
-        limit = root.get('count')
-    while len(images) < limit:
-        count = addGelbooruPosts(root, url)
-        if not(count):
-            break
-        req = request.urlopen(url + query + '&offset=' + str(len(images)))
-        root = ET.fromstring(req.read())
-        time.sleep(0.5)
-
-elif args.provider == 'rule34.paheal.net' or args.provider == 'r34':
-    print_err('Searching for images...')
-    ns = {'media': 'http://search.yahoo.com/mrss',
-          'atom': 'http://www.w3.org/2005/Atom'}
-
-    url = 'https://rule34.paheal.net/rss/images/'
-    tags = parse.quote(' '.join(args.tag), [])
-    req = request.urlopen(url + tags + '/1')
-    root = ET.fromstring(req.read())
-
-    limit = args.limit
-    if not(limit) and config['DEFAULT'].get('limit'):
-        limit = int(config['DEFAULT'].get('limit'))
-    if not(limit):
-        limit = 1e7
-
-    while len(images) < limit:
-        next = None
-        channel = root.find('channel')
-        count = addShimmiePosts(channel, url, ns)
-        if not(count):
-            break
-        for link in channel.findall('atom:link', ns):
-            if link.get('rel') == 'next':
-                next = parse.urljoin(url, link.get('href'))
-                time.sleep(0.5)
-                req = request.urlopen(next)
-                root = ET.fromstring(req.read())
-                break
-        if not(next):
-            break
-
-elif args.provider == 'shimmie.katawa-shoujo.com' or args.provider == 'ks':
-    print_err('Searching for images...')
-    ns = {'media': 'http://search.yahoo.com/mrss/',
-          'atom': 'http://www.w3.org/2005/Atom'}
-
-    url = 'https://shimmie.katawa-shoujo.com/rss/images/'
-    tags = parse.quote(' '.join(args.tag), [])
-    req = request.urlopen(url + tags + '/1')
-    root = ET.fromstring(req.read())
-
-    limit = args.limit
-    if not(limit) and config['DEFAULT'].get('limit'):
-        limit = int(config['DEFAULT'].get('limit'))
-    if not(limit):
-        limit = 1e7
-
-    while len(images) < limit:
-        next = None
-        channel = root.find('channel')
-        count = addShimmiePosts(channel, url, ns)
-        if not(count):
-            break
-        for link in channel.findall('atom:link', ns):
-            if link.get('rel') == 'next':
-                next = parse.urljoin(url, link.get('href'))
-                time.sleep(0.5)
-                req = request.urlopen(next)
-                root = ET.fromstring(req.read())
-                break
-        if not(next):
-            break
-
-else:
-    print_err('The specified provider is not supported.')
-
-# Download images
-print_err('%u images found' % len(images))
-for image in images:
-    print(image['url'])
-    if args.nd == False:
-        ireq = request.urlopen(image['url'])
-        basename = image['name']
-        if len(basename) > max_filename_len:
-            fn, ext = os.path.splitext(basename)
-            basename = textwrap.shorten(basename, width=max_filename_len, placeholder=ext)
-        filename = dir.rstrip(os.sep) + os.sep + basename
-        if not(os.path.isfile(filename)):
-            with open(filename, 'xb') as f:
-                f.write(ireq.read())
-        time.sleep(0.5)
+# Run application
+if __name__ == '__main__':
+    main()
